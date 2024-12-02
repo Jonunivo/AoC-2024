7583b4be3a0ac4e2a15606e6aad8e3a4835955d8
Mauro <mavogel@student.ethz.ch>
2024-12-02 09:57:12 +0100

    done Day2

--- Day2/main.cpp
+++ Day2/main.cpp
@@ -1,7 +1,7 @@
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
-#include <filesystem>
 #include <vector>
 #include <algorithm>
 
@@ -25,18 +25,83 @@ vector<string> readFileLineByLine(const std::string& fileName) {
     while (std::getline(inFile, line)) {
         // Process the line
         lines.push_back(line);
-        std::cout << line << std::endl;
+        //std::cout << line << std::endl;
     }
 
     inFile.close();
     return lines;
 }
 
+bool check_safety(vector<int> numbers){
+    bool safe = true;
+    if(numbers.size() < 2){
+        safe = false;
+    }
+    else if(numbers[0] == numbers[1]){
+        safe = false;
+    }
+    else if(numbers[0] < numbers[1]){
+        for(int i = 1; i<numbers.size(); i++){
+            int prev = numbers[i-1];
+            int curr = numbers[i];
+            if(curr - prev < 1 || curr - prev > 3){
+                safe = false;
+                break;
+            }
+        }
+    }
+    else{
+        for(int i = 1; i<numbers.size(); i++){
+            int prev = numbers[i-1];
+            int curr = numbers[i];
+            if(prev - curr < 1 || prev - curr > 3){
+                safe = false;
+                break;
+            }
+        }
+    }
+    return safe;
+}
+
 int main() {
     const std::string fileName = "input"; // Specify the file name
-    const std::string path = "C:/Users/uniho/OneDrive/Documents/AoC-2024/Day1/" + fileName + ".txt";
+    const std::string path = fileName + ".txt";
     std::ifstream inFile = openFile(path);
+    vector<string> reports = readFileLineByLine(path);
 
+    int counter = 0;
+    for(auto it = reports.cbegin(); it != reports.cend(); it++){
+        istringstream iss(*it);
+        vector<int> numbers; 
+        int num;
+        while(iss >> num){
+            numbers.push_back(num);
+        }
+
+        for(int i = 0; i<numbers.size(); i++){
+            vector<int> newNumbers;
+            for(int j = 0; j<numbers.size(); j++){
+                if(i != j){
+                    newNumbers.push_back(numbers[j]);
+                }
+            }
+            if(check_safety(newNumbers)){
+                counter++;
+                cout << "SAFE" << "\n";
+                break;
+            }
+            else{
+                if(i == numbers.size()-1){
+                    cout << "UNSAFE" << "\n";
+                }   
+                continue;
+            }
+
+        }
+
+    }
+
+    cout << counter;
 
     inFile.close();
 
